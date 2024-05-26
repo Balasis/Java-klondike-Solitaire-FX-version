ae6272625c0c0d56df46f8e36153bfd98995e9b9
Balasis_Ioannis <giovani1994a@gmail.com>
2024-05-26 22:31:58 +0300

    ... more private methods to break down private methods... this is getting out of hand

--- src/main/java/gr/athtech/balas/klondikesolitaireminigame/KlondikeSolitaireController.java
+++ src/main/java/gr/athtech/balas/klondikesolitaireminigame/KlondikeSolitaireController.java
@@ -1,10 +1,8 @@
 package gr.athtech.balas.klondikesolitaireminigame;
 
 import gr.athtech.balas.klondikesolitaireminigame.board.BoardCardsSlot;
-import gr.athtech.balas.klondikesolitaireminigame.board.FoundationSlot;
 import gr.athtech.balas.klondikesolitaireminigame.thedeck.Card;
 import gr.athtech.balas.klondikesolitaireminigame.thedeck.Deck;
-import gr.athtech.balas.klondikesolitaireminigame.thedeck.Suit;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -12,6 +10,7 @@ import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 
 import java.util.HashMap;
@@ -90,37 +89,61 @@ public class KlondikeSolitaireController {
             //tableSlots are also an array in model from 1 to 7
             boardSlotsMap.put(theGame.getATableSlot(i), tablesArray[i]);
         }
-        //   Populate the map with the initial bounds after the layout pass
-        Platform.runLater(this::updateStackPaneBounds);
-
         for(Map.Entry<Card,CardView> c:cardsMap.entrySet()){
           CardView cV=c.getValue();
+          addMouseListenersForDragAndDrop(cV);
+        }
+        //   Populate the map with the initial bounds after the layout pass
+        Platform.runLater(this::updateStackPaneBounds);
+    }
 
+    private void addMouseListenersForDragAndDrop(CardView cV){
+        addSetOnMouseClickListener(cV);
+        addSetOnMouseDraggedListener(cV);
+        addSetOnMouseReleased(cV);
+    }
 
-
-            cV.setOnMousePressed(event -> {
-            mouseX = event.getSceneX();
-            mouseY = event.getSceneY();
-            ImageView theDragger = (ImageView) event.getSource();
-            VBox theDraggerParentsParent = (VBox) theDragger.getParent().getParent();
-            theDraggerParentsParent.toFront();
+    //Click on card...
+    private void addSetOnMouseClickListener(CardView cV){
+        cV.setOnMousePressed(event -> {
+            //getMouseLocation of click to set the starting point of drag(used later for translate x,y)
+            setMouseCurrentLocation(event);
+            //changing the Z-order of Vbox (Anchor>Vbox>StackPane>CardView)  so the potential drag item(ImageView)
+            // won't be hidden by other stackpane. Used anchor so they won't affect view(absolute position)
+            cardViewParentOfParentInFront(event);
         });
+    }
 
-        // Event handler for mouse dragged (during drag)
-            cV.setOnMouseDragged(event -> {
+    private void setMouseCurrentLocation(MouseEvent e){
+        mouseX = e.getSceneX();
+        mouseY = e.getSceneY();
+    }
+
+    private void cardViewParentOfParentInFront(MouseEvent e){
+        ImageView theDragger = (ImageView) e.getSource();
+        VBox theDraggerParentsParent = (VBox) theDragger.getParent().getParent();
+        theDraggerParentsParent.toFront();
+    }
+
+    //...start moving mouse while holding click (drag)
+    private void addSetOnMouseDraggedListener(CardView cV){
+        cV.setOnMouseDragged(event -> {
             double offsetX = event.getSceneX() - mouseX;
             double offsetY = event.getSceneY() - mouseY;
 
-                cV.setTranslateX(cV.getTranslateX() + offsetX);
-                cV.setTranslateY(cV.getTranslateY() + offsetY);
+            cV.setTranslateX(cV.getTranslateX() + offsetX);
+            cV.setTranslateY(cV.getTranslateY() + offsetY);
 
             // Update stored mouse coordinates for the next drag event
             mouseX = event.getSceneX();
             mouseY = event.getSceneY();
             event.consume();
         });
+    }
 
-            cV.setOnMouseReleased(event -> {
+    //...release the mouse
+    private void addSetOnMouseReleased(ImageView cV){
+        cV.setOnMouseReleased(event -> {
             ImageView theDragger = (ImageView) event.getSource();
             // Get the bounds of imageView in the scene coordinate space
             Bounds imageViewBoundsInScene = theDragger.localToScene(theDragger.getBoundsInLocal());
@@ -151,19 +174,19 @@ public class KlondikeSolitaireController {
                     break;
                 }
             }
-
             if (!intersected) {
                 // Reset translation if not intersecting any target StackPane
                 theDragger.setTranslateX(0);
                 theDragger.setTranslateY(0);
             }
-
             mouseX = 0;
             mouseY = 0;
             event.consume();
         });
-     }
     }
+
+
+
 
 
     //record the bounds of the last card of each stack and reforms a full Map of them. (used for interception comparison)
@@ -187,11 +210,6 @@ public class KlondikeSolitaireController {
                 }
             }
         }
-
-    }
-
-
-    private void updateStackStatus() {
 
     }
 
