0fd3876af35e9c33eacfe65352c7c15bf1010198
Balasis_Ioannis <giovani1994a@gmail.com>
2024-05-26 22:54:11 +0300

    more commentary

--- src/main/java/gr/athtech/balas/klondikesolitaireminigame/KlondikeSolitaireController.java
+++ src/main/java/gr/athtech/balas/klondikesolitaireminigame/KlondikeSolitaireController.java
@@ -18,67 +18,52 @@ import java.util.Map;
 
 public class KlondikeSolitaireController {
     //Card Slots
-    @FXML
-    private StackPane deckSlot;
-    @FXML
-    private StackPane wasteSlot;
-    @FXML
-    private StackPane clubsFoundation;
-    @FXML
-    private StackPane diamondsFoundation;
-    @FXML
-    private StackPane heartsFoundation;
-    @FXML
-    private StackPane spadesFoundation;
-    @FXML
-    private StackPane tableSlot1;
-    @FXML
-    private StackPane tableSlot2;
-    @FXML
-    private StackPane tableSlot3;
-    @FXML
-    private StackPane tableSlot4;
-    @FXML
-    private StackPane tableSlot5;
-    @FXML
-    private StackPane tableSlot6;
-    @FXML
-    private StackPane tableSlot7;
+    @FXML private StackPane deckSlot;
+    @FXML private StackPane wasteSlot;
+    @FXML private StackPane clubsFoundation;
+    @FXML private StackPane diamondsFoundation;
+    @FXML private StackPane heartsFoundation;
+    @FXML private StackPane spadesFoundation;
+    @FXML private StackPane tableSlot1;
+    @FXML private StackPane tableSlot2;
+    @FXML private StackPane tableSlot3;
+    @FXML private StackPane tableSlot4;
+    @FXML private StackPane tableSlot5;
+    @FXML private StackPane tableSlot6;
+    @FXML private StackPane tableSlot7;
+    @FXML private AnchorPane containerAnchor;
+    //+52 Dynamically created ImageViews, as superclass of CardViews.(Cardviews extend ImageView)
+    //Each CardView has a "Card"(model class) field to have info on updating its image.
 
-    @FXML
-    private AnchorPane containerAnchor;
-
-    //The rest( the cards) are dynamically created due to their cardinality (52 cards)
-    //an array just for loop convenience all over the controller...
+    //Main game program, Maps to map view with model, mouse X|Y for drag and drop info, an array for loop convenience
+    private KlondikeSolitaireProgram theGame;
+    private final Map<Card, CardView> cardsMap;
+    private final Map<BoardCardsSlot, StackPane> boardSlotsMap = new HashMap<>();
+    private final Map<StackPane, Bounds> boundryOfLastImageViewInStacks = new HashMap<>();
+    private double mouseX, mouseY;
     StackPane[] tablesArray = {tableSlot1, tableSlot2, tableSlot3, tableSlot4, tableSlot5, tableSlot6, tableSlot7};
 
-    private KlondikeSolitaireProgram theGame;
-    //Map Cards to the CardViews(class which extend ImageView) that represents them.
-    private Map<Card, CardView> cardsMap;
-    //Map the BoardCardSlots to a StackPane
-    private Map<BoardCardsSlot, StackPane> boardSlotsMap = new HashMap<>();
-    //Holds the Bountry of the last Card of each StackPane(used in order to check intercept->Drag And Drop->Find desired Drop Stack)
-    private final Map<StackPane, Bounds> boundryOfLastImageViewInStacks = new HashMap<>();
-    //Location of the mouse related to scene, used in fake dragging process /change of translate x,y
-    private double mouseX, mouseY;
-
-
     public KlondikeSolitaireController() {
-        //Starting a new Game
+        //Init the gameProgram, get cardDeck, removing the jokers cards, populate cardMaps.
         theGame = new KlondikeSolitaireProgram();
-        //get The deck(deck is no longer in use after setting up the game;All cards get to the board)
         Deck deck = theGame.getDeck();
-        //Removing the jokers from the deck since we don't need them.
         deck.removeTheJokers();
-        //Map a CardView (extends ImageView) for each card of the game left(no Jokers).
         cardsMap = new HashMap<>();
         for (Card c : deck.getCards()) {
-            //Card view class holds only image info depending on card given, also a method to update it(hidden or revealed)
             cardsMap.put(c, new CardView(c));
         }
     }
 
     public void initialize() {
+        //populate boardSlotsMap,adding mouse listeners for drag and drop operations,
+        //adding a Bountry to be intercepted at each StackPane using the last childs(CardView) Bountry of it
+        populateTheBoardSlotMap();
+        addListenersToCardViews();
+        Platform.runLater(this::updateStackPaneBounds);
+    }
+
+    private void populateTheBoardSlotMap(){
+        //sadly couldn't think of a shortcut or a loop due to many differences
         boardSlotsMap.put(theGame.getDeckSlot(), deckSlot);
         boardSlotsMap.put(theGame.getWasteSlot(), wasteSlot);
         boardSlotsMap.put(theGame.getClubsFoundationSlot(), clubsFoundation);
@@ -89,12 +74,13 @@ public class KlondikeSolitaireController {
             //tableSlots are also an array in model from 1 to 7
             boardSlotsMap.put(theGame.getATableSlot(i), tablesArray[i]);
         }
+    }
+
+    private void addListenersToCardViews(){
         for(Map.Entry<Card,CardView> c:cardsMap.entrySet()){
-          CardView cV=c.getValue();
-          addMouseListenersForDragAndDrop(cV);
+            CardView cV=c.getValue();
+            addMouseListenersForDragAndDrop(cV);
         }
-        //   Populate the map with the initial bounds after the layout pass
-        Platform.runLater(this::updateStackPaneBounds);
     }
 
     private void addMouseListenersForDragAndDrop(CardView cV){
