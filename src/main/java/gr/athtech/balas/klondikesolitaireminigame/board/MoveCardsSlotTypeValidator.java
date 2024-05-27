f65f2722456778d6ecb06e56a0096d02fb0250db
Balasis_Ioannis <giovani1994a@gmail.com>
2024-05-27 22:08:01 +0300

    Fixed MoveCardsSlotTypeValidator (beautify)

--- src/main/java/gr/athtech/balas/klondikesolitaireminigame/board/MoveCardsSlotTypeValidator.java
+++ src/main/java/gr/athtech/balas/klondikesolitaireminigame/board/MoveCardsSlotTypeValidator.java
@@ -10,18 +10,23 @@ public class MoveCardsSlotTypeValidator {
 
     public MoveCardsSlotTypeValidator() {
         validMovesMap = new EnumMap<>(SlotType.class);//enumMap requires enumType..
-
         validMovesMap.put(SlotType.DECK, EnumSet.of(SlotType.WASTE));
         validMovesMap.put(SlotType.WASTE, EnumSet.of(SlotType.DECK));
         validMovesMap.put(SlotType.FOUNDATION, EnumSet.of(SlotType.WASTE, SlotType.TABLE));
         validMovesMap.put(SlotType.TABLE, EnumSet.of(SlotType.WASTE, SlotType.TABLE, SlotType.FOUNDATION));
     }
 
+    // API
     public boolean isMoveToCardSlotValid(BoardCardsSlot from, BoardCardsSlot to) {
-        if (from == null || to == null) {
-            return false;
-        }
-        Set<SlotType> validFromTypes = validMovesMap.get(to.getSlotType());
-        return validFromTypes != null && validFromTypes.contains(from.getSlotType());
+        Set<SlotType> validFromTypes = validMovesMap.getOrDefault(to.getSlotType(), null);
+        return from != null && validFromTypes != null && validFromTypes.contains(from.getSlotType());
+    }
+
+    // Overrides
+    @Override
+    public String toString() {
+        return "MoveCardsSlotTypeValidator{" +
+                "validMovesMap=" + validMovesMap +
+                '}';
     }
 }
